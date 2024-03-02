5af7be70b129e11f881b9086828467f3ddcb68c7
buxi <958685925@qq.com>
2024-03-02 15:05:12 +0800

    增加操作文档

--- MagicMan/src/main/java/com/buxi/magicman/controller/HealthController.java
+++ MagicMan/src/main/java/com/buxi/magicman/controller/HealthController.java
@@ -1,6 +1,13 @@
 package com.buxi.magicman.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HealthController {
 
+    @GetMapping("/magicman/healthcheck")
+    public String healthCheck(){
+        return "Hello, Magic Man";
+    }
 }
