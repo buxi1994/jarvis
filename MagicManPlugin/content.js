73c714feea3eb05e127457246c6c2f91d5a061ef
yangqianru <18298361451@163.com>
2024-03-04 00:26:01 +0800

    增加插件代码

---
+++ MagicManPlugin/content.js
@@ -0,0 +1,14 @@
+const pluginDiv = document.createElement('div');
+pluginDiv.id = "magic_man"
+document.body.appendChild(pluginDiv);
+
+var script = document.createElement('script');
+script.type='module';
+script.src = 'http://localhost/magicman-ui/plugin/version/assets/index-uBmcWP0N.js'
+document.head.appendChild(script);
+
+
+var link = document.createElement('link');
+link.rel='stylesheet';
+link.href = 'http://localhost/magicman-ui/plugin/version/assets/index-BAr5XKxs.css'
+document.head.appendChild(link);
