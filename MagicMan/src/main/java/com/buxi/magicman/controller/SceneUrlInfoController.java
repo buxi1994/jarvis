673ae4bced61b6ab9aec65087faac070e94e267e
yangqianru <18298361451@163.com>
2024-03-03 18:06:24 +0800

    调整目录结构

--- MagicMan/src/main/java/com/buxi/magicman/controller/SceneUrlInfoController.java
+++ MagicMan/src/main/java/com/buxi/magicman/controller/SceneUrlInfoController.java
@@ -4,6 +4,7 @@ import com.buxi.magicman.database.mapper.SceneUrlInfoMapper;
 import com.buxi.magicman.entity.SceneUrlInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
